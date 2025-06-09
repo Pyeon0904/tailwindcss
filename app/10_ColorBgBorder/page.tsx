0d4d68daa56ca9a8114e1a65baf8e0470b502f5a
Pyeon0904 <jjp2548@gmail.com>
2025-06-10 08:34:54 +0900

    feat: Color, Bg, Border

---
+++ app/10_ColorBgBorder/page.tsx
@@ -0,0 +1,9 @@
+export default function ColorBorder() {
+
+  return (
+    <>
+      <p className='bg-blue-700 text-white text-lg p-4 mb-6'>배경이 파란색, 글자색 흰색, 폰트사이즈 크게</p>
+      <p className='border-2 border-blue-600 rounded-full p-4'>파란색 보더와 둥근 모서리를 가진 보더를 적용!</p>
+    </>
+  );
+}

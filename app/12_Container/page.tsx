d56ec2f0facf45063e33ce2bc324b4ab6351d099
Onestar <63281505+Pyeon0904@users.noreply.github.com>
2025-06-10 21:44:28 +0900

    feat: 12.Container (#4)

---
+++ app/12_Container/page.tsx
@@ -0,0 +1,13 @@
+export default function Container() {
+
+  return (
+    <>
+      {/*<main className='container max-w-5xl p-4 bg-indigo-600 text-white h-screen mx-auto'>*/}
+      <main className='main-layout bg-indigo-600 text-white'>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque consectetur distinctio, doloremque dolorum
+        facilis fugiat, fugit illo laudantium nihil omnis quasi quibusdam quod sequi soluta, veniam vero voluptates.
+        Doloremque, suscipit?
+      </main>
+    </>
+  );
+}
